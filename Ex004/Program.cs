395325f84fd99a2715928e228afe5e873ade002c
Darya <darya.sizova1998@yandex.ru>
2022-08-12 16:43:35 +0400

    ex004

---
+++ Ex004/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int even = 2;
+
+while (number >= even)
+{
+    Console.WriteLine(even + " ");
+    even = even + 2;
+}

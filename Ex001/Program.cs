8be9cf43017885e54cc6a8a131f6ad15a64fa901
Darya <darya.sizova1998@yandex.ru>
2022-08-12 16:10:07 +0400

    ex001

---
+++ Ex001/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите 2 числа: ");
+Console.Write("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine());
+if (numberA > numberB)
+{
+    Console.WriteLine("Число " + numberA + " большее");
+    Console.Write("Число " + numberB + " меньшее");
+}
+
+else
+{
+    Console.WriteLine("Число " + numberB + " большее");
+    Console.WriteLine("Число " + numberA + " меньшее");
+}

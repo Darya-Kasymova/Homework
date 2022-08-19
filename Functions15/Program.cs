a9053e6507e5f90c816d76a86e34f7834020d02c
Darya <darya.sizova1998@yandex.ru>
2022-08-19 20:01:53 +0400

    15

---
+++ Functions15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введи цифру, чтобы узнать, является ли этот день недели выходным: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) {
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("да");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("нет такого дня недели");
+  }
+  else Console.WriteLine("нет");
+}
+
+CheckingTheDayOfTheWeek(dayNumber);

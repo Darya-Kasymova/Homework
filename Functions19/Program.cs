6ca7d5794ee7b8d49005c6d1ddc47c62ba580e0d
Darya <darya.sizova1998@yandex.ru>
2022-08-21 16:35:41 +0400

    19

---
+++ Functions19/Program.cs
@@ -0,0 +1,28 @@
+﻿int number = ReadInt("Введите пятизначное число: ");
+int num2 = number/1000;
+int num4 = number/10;
+ 
+if (number > 9999 && number < 100000)
+
+{if (number/10000 == Mod(number) && Mod(num2) == Mod(num4))
+{
+    Console.WriteLine(number + "-> да");
+}
+else  
+{ 
+  Console.WriteLine(number + "-> нет");  
+}
+}
+else 
+{
+    Console.WriteLine("Введено не пятизначное число");
+}
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int Mod(int num)
+{
+    return num%10;
+}
